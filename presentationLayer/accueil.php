9fad4d775e9551aca19486fb188d2a37ea05809d
Paul-Rancourt <2618825@edu.collegelacite.ca>
2017-03-20 13:29:57 -0400

    Navbar fixes
    Réparation du navbar

--- presentationLayer/accueil.php
+++ presentationLayer/accueil.php
@@ -1,69 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>ClubVid&eacute;o</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="ressources/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="ressources/css/small-business.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-<!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">
-                ClubVid&eacute;o
-            </a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
-
-<!-- Page Content -->
-<div class="container">
+<?php include('includes/head.php'); ?>
 
     <!-- Heading Row -->
     <div class="row">
@@ -72,9 +7,8 @@
         </div>
         <!-- /.col-md-8 -->
         <div class="col-md-4">
-            <h1>Business Name or Tagline</h1>
+            <h1>Club Vid&eacute;o</h1>
             <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-            <a class="btn btn-primary btn-lg" href="#">Call to Action!</a>
         </div>
         <!-- /.col-md-4 -->
     </div>
--- presentationLayer/includes/menu.php
+++ presentationLayer/includes/menu.php
@@ -8,12 +8,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">ClubVid&eacute;o</a>
+            <a class="navbar-brand" href="accueil.php">ClubVid&eacute;o</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-
+                <li>
+                    <a href="accueil.php">Accueil</a>
+                </li>
+                <li>
+                    <a href="navigation.php">Naviguer</a>
+                </li>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
