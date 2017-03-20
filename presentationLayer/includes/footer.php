8ba295119019b96d05036012b77b523a7b8848d8
nanouke <philippeg.vadnais@hotmail.com>
2017-03-20 12:58:19 -0400

    Decoupage des parties de la page

---
+++ presentationLayer/includes/footer.php
@@ -0,0 +1,24 @@
+</div>
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Your Website 2014</p>
+        </div>
+    </div>
+    <!-- /.row -->
+</footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="ressources/js/jquery.js"></script>
+
+
+<!-- Bootstrap Core JavaScript -->
+<script src="ressources/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ presentationLayer/login.php
@@ -0,0 +1,7 @@
+<?php include('includes/head.php'); ?>
+
+<div class="col-sm-12 col-md-6">
+
+</div>
+
+<?php include('includes/footer.php'); ?>
