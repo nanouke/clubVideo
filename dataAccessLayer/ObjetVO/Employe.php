7d779c83fb9e25318def8dcfb56f7a3cf107727c
Marc <marcandre.poulin96@gmail.com>
2017-03-20 13:01:44 -0400

    ObjetVO
    Objet représentant un Produit et un Employe

---
+++ dataAccessLayer/ObjetVO/Employe.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 3/20/2017
+ * Time: 12:58 PM
+ */
+class Employe
+{
+    // --- Variables
+    protected $employeID;
+    protected $nom;
+    protected $prenom;
+    protected $nomUtilisateur;
+    protected $motDePasse;
+
+    /**
+     * @return mixed
+     */
+    public function getEmployeID()
+    {
+        return $this->employeID;
+    }
+
+    /**
+     * @param mixed $employeID
+     */
+    public function setEmployeID($employeID)
+    {
+        $this->employeID = $employeID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomUtilisateur()
+    {
+        return $this->nomUtilisateur;
+    }
+
+    /**
+     * @param mixed $nomUtilisateur
+     */
+    public function setNomUtilisateur($nomUtilisateur)
+    {
+        $this->nomUtilisateur = $nomUtilisateur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMotDePasse()
+    {
+        return $this->motDePasse;
+    }
+
+    /**
+     * @param mixed $motDePasse
+     */
+    public function setMotDePasse($motDePasse)
+    {
+        $this->motDePasse = $motDePasse;
+    }
+
+    // --- Setters & Getters
+    
+}
---
+++ dataAccessLayer/ObjetVO/Produit.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Marc
+ * Date: 3/20/2017
+ * Time: 12:50 PM
+ */
+class Produit
+{
+    // --- Variables
+    protected $produitID;
+    protected $nom;
+    protected $categorie;
+    protected $description;
+    protected $prixLocation;
+    protected $disponible;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * @param mixed $categorie
+     */
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrixLocation()
+    {
+        return $this->prixLocation;
+    }
+
+    /**
+     * @param mixed $prixLocation
+     */
+    public function setPrixLocation($prixLocation)
+    {
+        $this->prixLocation = $prixLocation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDisponible()
+    {
+        return $this->disponible;
+    }
+
+    /**
+     * @param mixed $disponible
+     */
+    public function setDisponible($disponible)
+    {
+        $this->disponible = $disponible;
+    }
+
+
+}
