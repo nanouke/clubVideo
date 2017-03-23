a3e663e63d99eb0995bf598dbb602d02ec262859
Paul-Rancourt <2618825@edu.collegelacite.ca>
2017-03-23 09:36:10 -0400

    Fixes
    Ajout de texte

--- presentationLayer/accueil.php
+++ presentationLayer/accueil.php
@@ -8,7 +8,7 @@
         <!-- /.col-md-8 -->
         <div class="col-md-4">
             <h1>ClubLapointe</h1>
-            <p>Bienvenue chez ClubLapointe. L'endroit </p>
+            <p>Bienvenue chez ClubLapointe. L'endroit parfait pour tous vos besoins de vid&eacute;os, de jeux et de produits divers!</p>
         </div>
         <!-- /.col-md-4 -->
     </div>
@@ -20,7 +20,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="well text-center">
-                This is a well that is a great spot for a business tagline or phone number for easy access!
+                ClubLapointe a tout ce que vous avez besoin! Contactez-nous au 613-123-4567.
             </div>
         </div>
         <!-- /.col-lg-12 -->
@@ -32,19 +32,16 @@
         <div class="col-md-4">
             <h2>Heading 1</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-            <a class="btn btn-default" href="#">More Info</a>
         </div>
         <!-- /.col-md-4 -->
         <div class="col-md-4">
             <h2>Heading 2</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-            <a class="btn btn-default" href="#">More Info</a>
         </div>
         <!-- /.col-md-4 -->
         <div class="col-md-4">
             <h2>Heading 3</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-            <a class="btn btn-default" href="#">More Info</a>
         </div>
         <!-- /.col-md-4 -->
     </div>
