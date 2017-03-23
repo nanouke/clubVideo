3a3eede875c6de74e66e3d43b4edcbc7a87a4876
Marc-André Delorme <2637415@lacitec.on.ca>
2017-03-23 08:58:44 -0400

    Fichiers de trop

--- presentationLayer/navigation.php
+++ presentationLayer/navigation.php
@@ -47,44 +47,37 @@
 		        <!-- Projects Row -->
 
 		<?php
+		function folderSize ($dir)
+		{
+			$i = 0; 
+			if ($handle = opendir($dir)) {
+				while (($file = readdir($handle)) !== false){
+					if (!in_array($file, array('.', '..')) && !is_dir($dir.$file)) 
+						$i++;
+				}
+			}
+			
+			return $i;
+		}
+		
+		
+		$fsize = folderSize("ressources/images/imageFilm/");
 		$idx = 0;
-		for( $i = 0; $i < 4; $i++)
+		for( $i = 0; $i < $fsize; $i++)
 		{
 		
 		$idx++;
-		echo '<div class="row">';
+		if($i % 3 == 0)echo '<div class="row">';
             echo '<div class="col-md-4 portfolio-item">';
                 echo '<a href="#">';
-                    echo '<img class="img-responsive" src="ressources/images/film'.$idx.'.jpg" alt="">';
+                    echo '<img class="img-responsive" src="ressources/images/imageFilm/film'.$idx.'.jpg" alt="">';
                 echo '</a>';
                 echo '<h3>';
                     echo '<a href="#">Vid&eacute;o '.$idx.'</a>';
                 echo '</h3>';
                 echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>';
             echo '</div>';
-            
-			$idx++;
-			echo '<div class="col-md-4 portfolio-item">';
-                echo '<a href="#">';
-                    echo '<img class="img-responsive" src="ressources/images/film'.$idx.'.jpg" alt="">';
-                echo '</a>';
-                echo '<h3>';
-                    echo '<a href="#">Vid&eacute;o '.$idx.'</a>';
-                echo '</h3>';
-                echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>';
-            echo '</div>';
-			
-			$idx++;
-            echo '<div class="col-md-4 portfolio-item">';
-                echo '<a href="#">';
-                    echo '<img class="img-responsive" src="ressources/images/film'.$idx.'.jpg" alt="">';
-                echo '</a>';
-                echo '<h3>';
-                    echo'<a href="#">Vid&eacute;o '.$idx.'</a>';
-                echo '</h3>';
-                echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>';
-            echo '</div>';
-        echo '</div>';
+        if($i % 3 == 0 && $i != 0)echo '</div>';
 		echo 2+4;
 		}
 		?>
