bb60b65c9b43996262b9d8c9ec41ce1ffe9d0a2f
Paul-Rancourt <2618825@edu.collegelacite.ca>
2017-03-23 09:02:12 -0400

    fixes
    Changer le nom et l'image d'accueil et quelques fix

--- presentationLayer/includes/menu.php
+++ presentationLayer/includes/menu.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="accueil.php">ClubVid&eacute;o</a>
+            <a class="navbar-brand" href="accueil.php">ClubLapointe</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
