5190b6c0a12147d5571355e338f84e503ad36ae1
Marc-André Delorme <2637415@lacitec.on.ca>
2017-04-06 08:46:00 -0400

    Documentation et formatage de la page Navigation.

--- presentationLayer/navigation.php
+++ presentationLayer/navigation.php
@@ -1,64 +1,65 @@
 ﻿<?php include('includes/head.php'); ?>
 
-        <!-- Page Header -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Navigation
-                    <!--<small>Secondary Text</small>-->
-                </h1>
-            </div>
-        </div>
-
-		<?php
-		include('../dataAccessLayer/ObjetDAO/ProduitDAO.php');
-		include('../dataAccessLayer/ObjetVO/ProduitVO.php'); 
-		$dao = new ProduitDAO();
-		$liste = $dao->getListe();
-		
-		//print_r($liste);
-		
-		function folderSize ($dir)
-		{
-			$i = 0; 
-			if ($handle = opendir($dir)) {
-				while (($file = readdir($handle)) !== false){
-					if (!in_array($file, array('.', '..')) && !is_dir($dir.$file)) 
-						$i++;
-				}
-			}
-			
-			return $i;
-		}
-		
-		
-		$fsize = folderSize("ressources/images/imageFilm/");
-		$idx = 0;
-		//$fsize = 5;
-		echo '<div class="row">';
+<?php
+	include('../dataAccessLayer/ObjetDAO/ProduitDAO.php');
+	include('../dataAccessLayer/ObjetVO/ProduitVO.php'); 
+	
+	//création de l'objet DAO ayant accès à la base de donnée.
+	$dao = new ProduitDAO();
+	
+	//Aquisition des données dans la base de donnée.
+	$liste = $dao->getListe();
+	
+	//Index utilisé pour vérifier combiens il y a de produit par colonnes.
+	$idx = 0;
+	
+	echo '<div class="row">';
+	
+		//Parcourir la liste des produits dans la database.
 		foreach( $liste as $produit)
 		{
-		$idx++;
-		$description = $produit->getDescription();
-		$id = $produit->getProduitID();
-		$titre = $produit->getNom();
-		$nombreProduit = (int)$produit->getDisponible();
-		if($nombreProduit >= 1)$disponible = $nombreProduit." Disponible";
-		else $disponible = "Non disponible";
-		if($nombreProduit > 1) $disponible = $disponible. "s";
-		
-		
-            echo '<div class="col-md-4 portfolio-item">';
-                //echo '<a href="#">';
-                    echo '<img class="img-responsive" src="ressources/images/imageFilm/film'.$id.'.jpg" alt="" width="674" height="1000">';
-                //echo '</a>';
-                echo '<h3>';
-                    echo '<a href="#">'.$titre.'</br>('.$disponible.')</a>';
-                echo '</h3>';
-                echo '<p>'.$description.'</p>';
-            echo '</div>';
-		if($idx % 3 == 0){echo '</div>';echo '<div class="row">';}
-		}
-		?>
-    </div>
+			$idx++;
+			
+			//Aquisition des données
+			$description = $produit->getDescription();
+			$id = $produit->getProduitID();
+			$titre = $produit->getNom();
+			$nombreProduit = (int)$produit->getDisponible();
+			
+			//Déterminer si le produit est disponible.
+			if($nombreProduit >= 1)
+			{
+				$disponible = $nombreProduit." Disponible";
+			}
+			else
+			{
+				$disponible = "Non disponible";
+			}
+			
+			//Assurer de mettre le mot au pluriel si y en a plus que 1.
+			if($nombreProduit > 1)
+			{
+				$disponible = $disponible. "s";
+			}
+			
+			//Affichage de l'image selon l'ID du produit, le nom/titre et la description du produit.
+			echo '<div class="col-md-4 portfolio-item">';
+				echo '<img class="img-responsive" src="ressources/images/imageFilm/film'.$id.'.jpg" alt="'.$titre.'" width="674" height="1000">';
+				echo '<h3>';
+					echo '<a href="#">'.$titre.'</br>('.$disponible.')</a>';
+				echo '</h3>';
+				echo '<p>'.$description.'</p>';
+			echo '</div>';
+			
+			//changer de colonne lorsqu'il y a plus de 3 produits.
+			if($idx % 3 == 0)
+			{
+				echo '</div>';
+				echo '<div class="row">';
+			}
+		}	
+	echo '</div>';
+?>
+    
     <!-- Footer -->
     <?php include('includes/footer.php'); ?>
